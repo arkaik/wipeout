8ee6208d63fb67b9e276b02b26322a726e4bd17e
arkaik <falc@null.net>
2016-11-14 16:22:53 +0100

    scripts

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject target;
+	private Vector3 diff;
+
+	// Use this for initialization
+	void Start () {
+		diff = this.transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = target.transform.position + diff;
+	}
+}
