1e0da699817c3f1dba606ef800914db5e290927a
arkaik <falc@null.net>
2016-12-05 18:35:46 +0100

    Moarar things

---
+++ Assets/EndMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoToMenu() {
+		SceneManager.LoadScene("menu");
+	} 
+}
---
+++ Assets/FinishLineScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLineScript : MonoBehaviour {
+
+	public Canvas endMenu;
+	private int max_num_rounds = 1;
+
+	void OnTriggerExit(Collider other) {
+		endMenu.enabled = true;
+		other.attachedRigidbody.velocity = Vector3.zero;
+	}
+
+	// Use this for initialization
+	void Start () {
+		endMenu.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
