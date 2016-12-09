ad906329b0b96b7a07c67eefce4167374bcf45d1
jugon09 <camilocastro12@hotmail.com>
2016-12-09 20:48:40 +0100

    path creado y script para seguirlo

--- Assets/Script/IA.cs
+++ Assets/Script/IA.cs
@@ -1,85 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class IA : MonoBehaviour {
+	public editorPathScript PathToFollow;
+	public int currentPointID = 0;
+	public float reachDistance = 1.0f;
+	public float rotationSpeed = 5.0f;
+	public string pathName;
+	
+	Vector3 lastPosition;
+	Vector3 currentPosition;
 	
 	public Transform[] wayPointList;
  
      public int currentWayPoint = 0; 
      Transform targetWayPoint;
  
-     public float speed = 4f;
+     public float speed = 50f;
 	 
 	 public float rdp = 10f;
 
 	// Use this for initialization
 	void Start () {
-	
+		lastPosition = transform.position;
+		PathToFollow = GameObject.Find("Path").GetComponent<editorPathScript>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	         // check if we have somewere to walk
-         if(currentWayPoint < this.wayPointList.Length)
-         {
-			 targetWayPoint = wayPointList[currentWayPoint];
-             walk();
-         }
-	}
-	
-	void walk(){
-         // rotate towards the target
-         transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed*Time.deltaTime, 0.0f);
- 
-         // move towards the target
-         transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position,   speed*Time.deltaTime);
- 
-         if(dentroEsfera())
-         {
-             currentWayPoint ++ ;
-         }
-    }
-	
-	bool dentroEsfera() {
-	//x+y = rdp es la circumferencia si x^2+y^2 <= rdp^2 esta dentro sino fuera
-	Vector3 aux = new Vector3(transform.position.x - targetWayPoint.position.x,transform.position.y - targetWayPoint.position.y,transform.position.z - targetWayPoint.position.z);
-	return Mathf.Pow(aux.x,2) + Mathf.Pow(aux.y,2) + Mathf.Pow(aux.z,2) <= Mathf.Pow(rdp,2);
+			 if (currentPointID < PathToFollow.path_objs.Count) {
+				 Vector3 targetPosition = PathToFollow.path_objs[currentPointID].position;
+				 float distance = Vector3.Distance(targetPosition, transform.position);
+				 transform.position = Vector3.MoveTowards(transform.position,targetPosition,speed*Time.deltaTime);
+				 var rotation = Quaternion.LookRotation(targetPosition - transform.position);
+				 transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+				 if (distance <= reachDistance) {
+					 currentPointID = (currentPointID + 1) % PathToFollow.path_objs.Count;
+				 }
+			 }
+			
 	}
 	
 }
-
-/*public class IA : MonoBehaviour {
- 
-     // put the points from unity interface
-     
- 
-     // Use this for initialization
-     void Start () {
- 
-     }
-     
-     // Update is called once per frame
-     void Update () {
-         // check if we have somewere to walk
-         if(currentWayPoint < this.wayPointList.Length)
-         {
-             if(targetWayPoint == null)
-                 targetWayPoint = wayPointList[currentWayPoint];
-             walk();
-         }
-     }
- 
-     void walk(){
-         // rotate towards the target
-         transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed*Time.deltaTime, 0.0f);
- 
-         // move towards the target
-         transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position,   speed*Time.deltaTime);
- 
-         if(transform.position == targetWayPoint.position)
-         {
-             currentWayPoint ++ ;
-             targetWayPoint = wayPointList[currentWayPoint];
-         }
-     } 
- }*/
