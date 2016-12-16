00e5c9ca793d5a83899a223291ff7d0ae516cded
jugon09 <camilocastro12@hotmail.com>
2016-12-16 18:25:43 +0100

    nuevos modelos y script de semaforo funcinando

---
+++ Assets/Script/SemaforoScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SemaforoScript : MonoBehaviour {
+	
+	GameObject circulo1,circulo2,circulo3;
+	public bool empezar;
+	private int contador;
+
+	// Use this for initialization
+	void Start () {
+		empezar = false;
+		contador = 0;
+		circulo1 = GameObject.Find("Circle001");
+		circulo2 = GameObject.Find("Circle002");
+		circulo3 = GameObject.Find("Circle003");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!empezar) {
+			if (contador >= 0 && contador < 100) {
+				circulo1.GetComponent<Renderer>().material.color = Color.red;
+			}
+			else if (contador >= 100 && contador < 200) {
+				circulo2.GetComponent<Renderer>().material.color = Color.red;
+			}
+			else if (contador >= 200 && contador < 300) {
+				circulo3.GetComponent<Renderer>().material.color = Color.red;
+			}
+			else {
+				circulo1.GetComponent<Renderer>().material.color = Color.green;	
+				circulo2.GetComponent<Renderer>().material.color = Color.green;	
+				circulo3.GetComponent<Renderer>().material.color = Color.green;
+				empezar = true;
+			}
+			++contador;
+		}
+	}
+}
